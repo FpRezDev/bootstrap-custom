2c295d294f3d05f9742d3fd88a9a1fe2ac50b5ee
FpRezDev <fprez.developer@outlook.com>
2020-05-25 11:45:52 -0600

    implement gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,37 @@
-const { buildBootstrapJs, buildBootstrapCss, buildBootstrap } = require("./build/bootstrap");
+const { parallel } = require('gulp');
+const builder = require('gulp-rollup-bootstrap-custom');
+const config = require('./build.config');
 const del = require('del');
-exports.build = buildBootstrap;
-exports.buildCss = buildBootstrapCss;
-exports.buildJs = buildBootstrapJs;
-exports.cleanDist = function() {
-  return del('./dist');
+
+const buildCss = () => {
+  return builder.buildCss(config.cssConfig);
+};
+
+const buildJs = () => {
+  return builder.buildJs(config.jsConfig);
+};
+
+const buildLibs = () => {
+  return builder.buildLibs(config.libsConfig);
+};
+
+const cleanCss = () => {
+  return del('wwwroot/css/');
 }
+
+const cleanJs = () => {
+  return del('wwwroot/js/');
+}
+
+const cleanLibs = () => {
+  return del('wwwroot/libs/');
+}
+
+exports.buildCss = buildCss;
+exports.buildJs = buildJs;
+exports.buildLibs = buildLibs;
+exports.build = parallel(buildCss, buildJs, buildLibs);
+exports.cleanCss = cleanCss;
+exports.cleanJs = cleanJs;
+exports.cleanLibs = cleanLibs;
+exports.clean = parallel(cleanCss, cleanJs, cleanLibs);
